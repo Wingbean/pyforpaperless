afa4730b12b0d6f0d2ed99691daaa1612dcb7d45
Wingbean <taeey.com@gmail.com>
2025-04-21 19:03:43 +0700

    create db model Job Position

--- firstweb/myapp/models.py
+++ firstweb/myapp/models.py
@@ -4,3 +4,17 @@ class Job(models.Model):
     fullname = models.CharField(max_length=255)
     tel = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
+
+    # ทำให้หลังจาก เพิ่มข้อมูลใน form มันจะแสดงเป็นชื่อ fullname
+    # ไม่ต้อง makemigration
+    def __str__(self):
+        return self.fullname
+
+class Position(models.Model):
+    title = models.CharField(max_length=255)
+    salary = models.IntegerField(default=10000) # ค่าเริ่มต้น 10000
+    manager = models.CharField(max_length=255) #ใครเป็นผู้จัดการ
+    description = models.TextField(null=True, blank=True) #ไม่บังคับกรอก
+    
+    def __str__(self):
+        return self.title
