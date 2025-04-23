11a684a98dd5d8aa4e9ebe2443b95d8b4b486238
Wingbean <taeey.com@gmail.com>
2025-04-23 22:57:17 +0700

    save submit to model

--- firstweb/myapp/views.py
+++ firstweb/myapp/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse # add line
+from .models import Job # add line จาก models.py นำ class Job เข้ามา
 
 # start create fn for myapp\urls.py
 
 def Home(request):
+    if request.method == 'POST' : 
+        data = request.POST.copy() # data เป็น Dict ที่มาจากการ submit form
+        print('DATA: ', data)
+        fullname = data.get('fullname')
+        tel = data.get('tel')
+        position = data.get('position')
+        print('Fullname: ', fullname)
+        print('Tel: ', tel)
+        print('position: ', position)
+        # ดึง model Job มา save ของที่ submit เข้ามา
+        newjob = Job()
+        newjob.fullname = fullname
+        newjob.tel = tel
+        newjob.position = position
+        newjob.save() #ใช้ method save() ที่ inherit มาจาก "from django.db import models"
+
     #return HttpResponse('<h1>Hello World from DataSloth</h1>')
     return render(request, 'myapp/home.html')
