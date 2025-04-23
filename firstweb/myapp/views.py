f53580f5f21f1eb0563f519d51fcfb10b957e05e
Wingbean <taeey.com@gmail.com>
2025-04-23 14:29:57 +0700

    edit setting and view

--- firstweb/myapp/views.py
+++ firstweb/myapp/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse # add line
 # start create fn for myapp\urls.py
 
 def Home(request):
-    return HttpResponse('<h1>Hello World from DataSloth</h1>')
+    #return HttpResponse('<h1>Hello World from DataSloth</h1>')
+    return render(request, 'myapp/home.html')
