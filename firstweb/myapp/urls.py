63d5f60aa50896284d0957c8f07f26ddebce6c8e
Wingbean <taeey.com@gmail.com>
2025-04-27 05:26:39 +0700

    B edit base add nav bar

--- firstweb/myapp/urls.py
+++ firstweb/myapp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('table-job/', table_job, name='table-job'),
+    path('table-job/', table_job, name='table-job'), #name คือสิ่งที่เราจะไปอ้างอิงทั้งเวบ
 ]
